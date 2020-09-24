e2e6a1330931e6fc60b897b46135cd2a7532cba1
mohammedAlghazali <hamood-monzer11@hotmail.com>
2020-09-24 21:56:37 +0300

    make style for info section in home page

--- src/components/Home/Info/index.js
+++ src/components/Home/Info/index.js
@@ -1,27 +1,46 @@
 import React from "react"
 import { Link } from "gatsby"
+import { Row, Col, Button } from "antd"
+
+import "./style.css"
 import Title from "../../Global/Title"
 
 const Info = () => {
   return (
-    <section style={{ height: "250px", border: "1px solid #000" }}>
-      <div className="container">
-        <Title title="Mohammed Alghazli" />
-        <div className="row">
-          <div>
-            <p>
-              lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
-              lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
-              lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
-              lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem
-            </p>
-            <Link to="/about">
-              <button>About</button>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </section>
+    <Row className="home__info" justify="center">
+      <Col span={24}>
+        <Title title="Our Story" />
+      </Col>
+
+      <Col
+        xs={18}
+        sm={16}
+        md={16}
+        lg={14}
+        xl={14}
+        className="home__info__paragraph"
+      >
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tincidunt
+          lacus sed justo lacinia faucibus. Aenean aliquam arcu ut arcu
+          ultricies, vitae ullamcorper enim placerat. Vestibulum id quam
+          finibus, vulputate quam id, imperdiet nulla. Vivamus accumsan
+          elementum mi, euismod ullamcorper justo malesuada a. Nunc turpis
+          tortor, finibus vel massa eu, volutpat molestie risus. Nunc eget
+          tempor ante, nec lacinia justo. Donec mollis, odio non lacinia
+          ultricies, arcu diam finibus eros, ut viverra erat justo blandit sem.
+          Aenean semper nunc justo, vitae blandit nibh viverra vel. Praesent
+          dictum elementum tortor nec molestie. Aenean nec placerat quam.
+          Suspendisse consequat diam ut leo.
+        </p>
+      </Col>
+
+      <Col span={24}>
+        <Link to="/about">
+          <Button type="primary">About Page</Button>
+        </Link>
+      </Col>
+    </Row>
   )
 }
 
